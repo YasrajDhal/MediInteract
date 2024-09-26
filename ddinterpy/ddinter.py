87c3571af72e8379753b38bc3d4058741b94a23d
Yasraj Dhal <yasrajdhal2922@gmail.com>
2024-09-26 19:04:14 +0530

    fixed bugs

--- ddinterpy/ddinter.py
+++ ddinterpy/ddinter.py
@@ -1,9 +1,15 @@
-ATLAS_URI = "mongodb+srv://hitesh19426:Yaoj5LYhWuXKBFkr@cluster0.lmtgovu.mongodb.net/?retryWrites=true&w=majority"
+ATLAS_URI = "mongodb+srv://yasraj:yasraj1234@personalprojects.fbdyv.mongodb.net/?retryWrites=true&w=majority"
 import pymongo
 
 client = pymongo.MongoClient(ATLAS_URI)
-mydb = client["ddinter_demo"]
+mydb = client["PersonalProjects"]
 mycollection = mydb["interactions"]
 
 
 mycollection.create_index([("Drug_A", pymongo.ASCENDING), ("Drug_B", pymongo.ASCENDING)], name="interaction_index")
+
+import pandas as pd
+data = pd.read_csv("D:/Projects/ddinter-main/ddinterpy/ddinter_downloads_code_A.csv")
+data_dict = data.to_dict(orient="records")
+
+mycollection.insert_many(data_dict)
